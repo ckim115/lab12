fcfa8dc2d08d092a031c41cba22ae75731cd1d5c
neenamcdermott <neena.mcdermott@gmail.com>
2022-11-18 13:20:31 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -43,20 +43,23 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (targetName.equals(name))
+            if (this.name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
-            	return child.getNodeWithName(targetName);
+                
+            	// then that's the node we're looking for. Return it.
+            	if (child.getNodeWithName(targetName) != null) 
+            		return child;
             }
             
             // Not found anywhere.
             return null;
         }
+        
         
         
         // Returns a list of ancestors of this TreeNode, starting with this node’s parent and
@@ -71,7 +74,7 @@ public class FamilyTree
             // recent to ancient. Expect a question about this on the final exam.
             TreeNode node = this;
             while(node.parent != null) {
-            	ancestors.add(node);
+            	ancestors.add(node.parent);
             	node = node.parent;
             }
 
@@ -117,8 +120,8 @@ public class FamilyTree
 
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
+		FileReader fr = new FileReader(treeFile);
+		BufferedReader br = new BufferedReader(fr);
 		String line;
 		while ((line = br.readLine()) != null)
 			addLine(line);
@@ -133,16 +136,24 @@ public class FamilyTree
 	//
 	private void addLine(String line) throws TreeException
 	{
+		int index = -1;
+		for (int i = 0; i < line.length(); i++) {
+			if (line.charAt(i) == ':' ) {
+				index = i;
+				break;
+			}
+
+}
 		// Extract parent and array of children.
-		int colonIndex = ?? should be the index of the colon in line.
+		int colonIndex = index;// should be the index of the colon in line.
 		if (colonIndex < 0)
-			?? throw a TreeException with a useful message
-		String parent = ?? The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
-				           class java.util.String, method substring(), if you need guidance.
-		String childrenString = ?? The substring of line that starts just after colonIndex and goes through the end of
-				                   the line. You'll use a different version of substring().
-		String[] childrenArray = ?? Call childrenString.split(). Check the API for details. The result will be an array
-				                    of strings, with the separating commas thrown away.
+			throw new TreeException("not good index");// throw a TreeException with a useful message
+		String parent = line.substring(0, colonIndex); //The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
+				           //class java.util.String, method substring(), if you need guidance.
+		String childrenString = line.substring(colonIndex + 1, line.length()); //The substring of line that starts just after colonIndex and goes through the end of
+				                   //the line. You'll use a different version of substring().
+		String[] childrenArray = childrenString.split(","); //Call childrenString.split(). Check the API for details. The result will be an array
+				                    //of strings, with the separating commas thrown away.
 		
 		// Find parent node. If root is null then the tree is empty and the
 		// parent node must be constructed. Otherwise the parent node should be 
@@ -152,13 +163,22 @@ public class FamilyTree
 			parentNode = root = new TreeNode(parent);
 		else
 		{
-			parentNode = root.?????  There's a method in Node that searches for a named node. 
-			??? If the parent node wasn't found, there must have been something wrong in the 
-				data file. Throw an exception.
+			parentNode = root.getNodeWithName(parent); 
+			// There's a method in Node that searches for a named node. 
+			
+			if (parentNode == null) {
+				throw new TreeException("null parent");
+			}
+			//??? If the parent node wasn't found, there must have been something wrong in the 
+				//data file. Throw an exception.
 		}
+		for (String child : childrenArray) {
+			parentNode.addChild(new TreeNode (child));
 		
 		// Add child nodes to parentNode.
-		?? For each name in childrenArray, create a new node and add that node to parentNode.
+		//?? For each name in childrenArray, create a new node and add that node to parentNode.
+		
+		}
 	}
 	
 	
@@ -171,16 +191,20 @@ public class FamilyTree
 	TreeNode getMostRecentCommonAncestor(String name1, String name2) throws TreeException
 	{
 		// Get nodes for input names.
-		TreeNode node1 = root.???		// node whose name is name1
-		if (node1 == null)
-			??? Throw a TreeException with a useful message
-		TreeNode node2 = root.???		// node whose name is name2
-		if (node2 == null)
-			??? Throw TreeException with a useful message
+		TreeNode node1 = root.getNodeWithName(name1); 		// node whose name is name1
+		if (node1 == null) {
+			throw new TreeException("not good name");
+		}
+			//??? Throw a TreeException with a useful message
+		TreeNode node2 = root.getNodeWithName(name2);		// node whose name is name2
+		if (node2 == null) {
+			throw new TreeException("not good name");
+			//??? Throw TreeException with a useful message
+		}
 		
 		// Get ancestors of node1 and node2.
-		ArrayList<TreeNode> ancestorsOf1 = ???
-		ArrayList<TreeNode> ancestorsOf2 = ???
+		ArrayList<TreeNode> ancestorsOf1 = node1.collectAncestorsToList();
+		ArrayList<TreeNode> ancestorsOf2 = node2.collectAncestorsToList();
 		
 		// Check members of ancestorsOf1 in order until you find a node that is also
 		// an ancestor of 2. 
